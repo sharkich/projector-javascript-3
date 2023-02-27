31b8673ad41a8108e3c9be2b704022e1b704fcf2
Artem Koziar <koziar.artem@gmail.com>
2023-02-27 20:15:13 +0200

    add comments

--- the-perfect-name-of-the-file.js
+++ the-perfect-name-of-the-file.js
@@ -1,1 +1,21 @@
-console.log('Hello wwworld!');
+'use strict';
+
+// bla bla bla
+console.log(1);
+
+// bla bla bla
+console.log(3);
+console.log(4); // bla bla bla console.log(5);
+
+// TODO: Fix it(!)
+console.log(5);
+
+/*
+    bla bla bla
+    bla bla bla
+    console.log(5);
+*/
+
+// 1. Readability
+// 2. Debuging
+// 3. TODO
