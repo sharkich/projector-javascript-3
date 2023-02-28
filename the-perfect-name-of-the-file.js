b8eec3febb74b239f664a2d740bda7b6e172db82
Artem Koziar <koziar.artem@gmail.com>
2023-02-28 12:29:05 +0200

    copy

--- the-perfect-name-of-the-file.js
+++ the-perfect-name-of-the-file.js
@@ -1,25 +1,6 @@
 'use strict';
 
-var zxc = 322;
 
-console.log('zxc => ', zxc);
-
- {
-    let a;
-    console.log('a -> ', a);
-    a = 1;
-    console.log('a -> ', a);
-
-    const _$b_$1_$2_ABC_3$ = 2;
-
-    var zxc = 322;
-
-    const a1 = 1;
-    const A1 = 1;
-
-    var zxc = 123;
- }
-
- {
-    console.log('zxc => ', zxc);
- }
+const a = 123;
+const b = ''+a;
+console.log(b);
