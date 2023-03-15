2131dcafd206cb1c326d2414df76e3692e06b785
Anatolii <anatorusan@gmail.com>
2023-03-16 00:16:09 +0200

    updated time.js

--- time.js
+++ time.js
@@ -1,33 +1,16 @@
 'use strict'
-function durationBetweenDates (initalDate = "14 Jan 1992", endDate = "11 March 2023", timeUnit = "days") {
-    let duration;
+function durationBetweenDates (initalDateStr = "14 Jan 1992", endDateStr = "11 March 2023", timeUnit = "days") {
     const timeUnitsTable = {
         seconds: 1000,
         minutes: 60000,
         hours: 3600000,
         days: 86400000
     }
-    const milisecInitalDate = new Date(initalDate);
-    const milisecEndDate = new Date(endDate);
-    let milisecDif = milisecEndDate - milisecInitalDate;
-    if (milisecDif < 0) {
-        milisecDif *= -1;
-    };
-    switch (true) {
-        case timeUnit === "seconds":
-            duration = milisecDif / timeUnitsTable.seconds;
-            break;
-        case timeUnit === "minutes":
-            duration = milisecDif / timeUnitsTable.minutes;
-            break;
-        case timeUnit === "hours":
-            duration = milisecDif / timeUnitsTable.hours;
-            break;
-        default:
-            duration = milisecDif / timeUnitsTable.days;
-            break;
-    }
+    const InitalDateObj = new Date(initalDateStr);
+    const EndDateObj = new Date(endDateStr);
+    const DateDif = Math.abs(EndDateObj - InitalDateObj);
+    const duration = DateDif / timeUnitsTable[timeUnit];
     return `${duration} ${timeUnit}`;
 }
 
-console.log(durationBetweenDates('14 Jan 1992', '11 March 2023', 'minutes'));
+console.log(durationBetweenDates('15 Jan 1992', '13 jan 1992', 'hours'));
