c9064d70b19f05b9760261c055e2d5194375ae10
Anatolii <anatorusan@gmail.com>
2023-03-16 18:53:56 +0200

    updated recursive_iterativeOddSum.js

--- recursive_iterativeOddSum.js
+++ recursive_iterativeOddSum.js
@@ -1,20 +1,21 @@
 'use strict'
 const recursiveOddSumTo = (number) => {
-    if (number === 1) {return 1}
+    if (number === 1) {
+      return 1;
+    }
     if (number % 2 === 1) {
       return number + recursiveOddSumTo(number - 2);
-    } else {
-      return recursiveOddSumTo(number - 1);
-    }
+    } 
+    return recursiveOddSumTo(number - 1);
 }
 
 const iterativeOddSumTo = (number) => {
-    if (number === 1) {return 1};
+    if (number === 1) {
+      return 1;
+    }
     let sum = 0;
-    let currentOddNumber = 1;
-    while(currentOddNumber <= number) {
-        sum += currentOddNumber;
-        currentOddNumber += 2;
+    for(let currNum = 1; currNum <= number; currNum += 2) {
+      sum += currNum;
     }
     return sum;
 }
