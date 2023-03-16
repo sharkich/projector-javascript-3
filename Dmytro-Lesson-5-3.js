f561a29775a62e4273070a6b697820c0086839fa
Bloodovich <drybac@gmail.com>
2023-03-16 19:25:30 +0200

    update lesson 5-3

---
+++ Dmytro-Lesson-5-3.js
@@ -0,0 +1,23 @@
+function recursiveOddSumTo(number) {
+    if (number <= 0) {
+      return 0;
+    } else if (number % 2 === 1) {
+      return number + recursiveOddSumTo(number - 2);
+    } else {
+      return recursiveOddSumTo(number - 1);
+    }
+  }
+  
+  function iterativeOddSumTo(number) {
+    let sum = 0;
+    for (let i = 1; i <= number; i += 2) {
+      sum += i;
+    }
+    return sum;
+  }
+
+  console.log(recursiveOddSumTo(1));
+  console.log(recursiveOddSumTo(10));
+
+  console.log(iterativeOddSumTo(1));
+  console.log(iterativeOddSumTo(10));
