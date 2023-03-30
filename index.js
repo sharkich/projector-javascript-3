f22fe33e3e2d7acc26df57cd163cc711c6bb00ed
Artem Koziar <akoziar@griddynamics.com>
2023-03-30 16:41:17 +0300

    add js module

---
+++ index.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+
+import './index2.js';
+
+/*
+  Google: todo list html css template
+  https://freefrontend.com/bootstrap-to-do-lists/
+  https://bbbootstrap.com/snippets/awesome-todo-list-template-25095891
+ */
+
+console.log('Workshop 1');
