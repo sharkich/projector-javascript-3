fd42e46018e88aca54a49db33c8d14a77a86dec8
Artem Koziar <koziar.artem@gmail.com>
2023-03-31 20:22:23 +0300

    shown list with pretty ui

--- renderList.js
+++ renderList.js
@@ -5,7 +5,16 @@ export const renderList = (tasksList) => {
 
     tasksList.forEach((task) => {
         const taskElement = document.createElement('li');
-        taskElement.innerHTML = `<p>${task}</p>`;
+        taskElement.innerHTML = `
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input class="checkbox" type="checkbox">
+                    ${task}
+                    <i class="input-helper"></i>
+                </label>
+            </div>
+            <a class="remove mdi mdi-close-circle-outline"></a>
+        `;
         listElement.append(taskElement);
     });
 
