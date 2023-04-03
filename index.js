210f62c4448514373fab104f944b63d5edadd800
Artem Koziar <koziar.artem@gmail.com>
2023-04-03 19:53:34 +0300

    workshop-2: handle numbers click

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 `use strict`;
 
+const inputElement = document.querySelector('.input');
+const numbersElements = document.querySelectorAll('.number');
+
+const handleNumberClick = (event) => {
+    event.preventDefault();
+    inputElement.value += event.target.innerHTML;
+};
+
 const startApp = () => {
     console.log('Workshop2 startApp');
+
+    numbersElements.forEach(numberElement => numberElement.addEventListener('click', handleNumberClick))
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
