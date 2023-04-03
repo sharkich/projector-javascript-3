72f7a7ce67dec3cb1e22813369c8600892a16ee2
Artem Koziar <koziar.artem@gmail.com>
2023-04-03 19:56:03 +0300

    workshop-2: handle clear click

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 
 const inputElement = document.querySelector('.input');
 const numbersElements = document.querySelectorAll('.number');
+const clearElement = document.querySelector('.clear');
 
 const handleNumberClick = (event) => {
-    event.preventDefault();
     inputElement.value += event.target.innerHTML;
+};
+
+const handleClearClick = () => {
+    inputElement.value = '';
 };
 
 const startApp = () => {
     console.log('Workshop2 startApp');
 
-    numbersElements.forEach(numberElement => numberElement.addEventListener('click', handleNumberClick))
+    numbersElements.forEach(numberElement => numberElement.addEventListener('click', handleNumberClick));
+    clearElement.addEventListener('click', handleClearClick);
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
