73af240cbb40dfc07b16830b8219ee81eb124a2c
Artem Koziar <koziar.artem@gmail.com>
2023-04-03 20:05:03 +0300

    workshop-2: handle operators click

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const inputElement = document.querySelector('.input');
 const numbersElements = document.querySelectorAll('.number');
+const operatorsElements = document.querySelectorAll('.operator');
 const clearElement = document.querySelector('.clear');
 
 const handleNumberClick = (event) => {
@@ -12,11 +13,16 @@ const handleClearClick = () => {
     inputElement.value = '';
 };
 
+const handleOperatorClick = (event) => {
+    inputElement.value += event.target.innerHTML;
+};
+
 const startApp = () => {
     console.log('Workshop2 startApp');
 
     numbersElements.forEach(numberElement => numberElement.addEventListener('click', handleNumberClick));
     clearElement.addEventListener('click', handleClearClick);
+    operatorsElements.forEach(operatorElement => operatorElement.addEventListener('click', handleOperatorClick));
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
