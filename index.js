beb9c7bdf5ff071890203fe7ffda44d8a9ac381d
Artem Koziar <koziar.artem@gmail.com>
2023-04-07 14:17:58 +0300

    workshop-3: init

--- index.js
+++ index.js
@@ -1,61 +1,7 @@
 `use strict`;
 
-import { doCalculation } from "./calc.js";
-
-const inputElement = document.querySelector('.input');
-const numbersElements = document.querySelectorAll('.number');
-const operatorsElements = document.querySelectorAll('.operator');
-const clearElement = document.querySelector('.clear');
-const resultElement = document.querySelector('.result');
-
-const handleNumberClick = (event) => {
-    const lastIndex = inputElement.value.length - 1;
-    const lastChar = inputElement.value[lastIndex];
-    if (event.target.innerHTML === '.') {
-        if (!inputElement.value) {
-            inputElement.value = '0.';
-            return;
-        }
-        if (lastChar === '.') {
-            return;
-        }
-    }
-    inputElement.value += event.target.innerHTML;
-};
-
-const handleClearClick = () => {
-    inputElement.value = '';
-};
-
-const handleOperatorClick = (event) => {
-    if (inputElement.value === '') {
-        inputElement.value = '0' + event.target.innerHTML;
-        return;
-    }
-    const NUMBERS_SYMBOLS = '01234567890.';
-    const lastIndex = inputElement.value.length - 1;
-    const lastChar = inputElement.value[lastIndex];
-    if (!NUMBERS_SYMBOLS.includes(lastChar)) {
-        inputElement.value = inputElement.value.slice(0, lastIndex);
-    }
-    inputElement.value += event.target.innerHTML;
-};
-
-const handleResultClick = () => {
-    // the main logic
-    const prepearedString = inputElement.value
-        .replace(/÷/g, '/')
-        .replace(/×/g, '*');
-    inputElement.value = doCalculation(prepearedString);
-};
-
 const startApp = () => {
-    console.log('Workshop2 startApp');
-
-    numbersElements.forEach(numberElement => numberElement.addEventListener('click', handleNumberClick));
-    clearElement.addEventListener('click', handleClearClick);
-    operatorsElements.forEach(operatorElement => operatorElement.addEventListener('click', handleOperatorClick));
-    resultElement.addEventListener('click', handleResultClick);
+    console.log('Workshop3 startApp');
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
