52fa9548953bb91173e64dcbafc08f420f9628ea
Artem Koziar <koziar.artem@gmail.com>
2023-04-07 20:09:51 +0300

    workshop-3: hide ball

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ let currentBallPosition = null;
 
 const getRandomIndex = () => Math.floor(Math.random() * 3);
 
-const putBallToRandomThimble = () => {
-    if (currentBallPosition !== null) {
-        thimblesElements[currentBallPosition].classList.remove('thimbleUp');
-    }
-    currentBallPosition = getRandomIndex();
-    ballElement.setAttribute('class', `thimbleBallPosition${currentBallPosition}`);
-};
-
 const showBall = () => {
     thimblesElements[currentBallPosition].classList.add('thimbleUp');
+};
+
+const hideBall = () => {
+    if (currentBallPosition === null) {
+        return;
+    }
+    thimblesElements[currentBallPosition].classList.remove('thimbleUp');
+};
+
+const putBallToRandomThimble = () => {
+    hideBall();
+    currentBallPosition = getRandomIndex();
+    ballElement.setAttribute('class', `thimbleBallPosition${currentBallPosition}`);
 };
 
 const handlePlay = () => {
     console.log('handlePlay');
     putBallToRandomThimble();
     showBall();
+    setTimeout(() => hideBall(), 500);
 };
