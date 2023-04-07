46243e0a4a53b6dbd253e69d16be1ba6c723d417
Artem Koziar <koziar.artem@gmail.com>
2023-04-07 19:54:57 +0300

    workshop-3: initial move ball

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 `use strict`;
 
-const startApp = () => {
-    console.log('Workshop3 startApp');
+const ballElement = document.getElementById('thimbleBall');
+
+const getRandomIndex = () => Math.floor(Math.random() * 3);
+
+const putBallToRandomThimble = () => {
+    const index = getRandomIndex();
+    ballElement.setAttribute('class', `thimbleBallPosition${index}`);
 };
 
-document.addEventListener('DOMContentLoaded', startApp);
+const showBall = () => {};
+
+const handlePlay = () => {
+    console.log('handlePlay');
+    putBallToRandomThimble();
+    showBall();
+};
