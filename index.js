d10ef822c6e87dea5ccdb605a92929ec6fe2f440
Artem Koziar <koziar.artem@gmail.com>
2023-04-10 20:13:07 +0300

    oop-1: comm 2: GreenApples

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 `use strict`;
 
 class GreenApple {
+    constructor(size, isInTree) {
+        this.size = size;
+        this.isInTree = isInTree;
+    }
+
+    fall() {
+        if (this.isInTree) {
+            console.log('Apple fell');
+        } else {
+            console.log('Apple is not in tree');
+        }
+    }
 }
 
-const a = new GreenApple();
+const a = new GreenApple('small', true);
+const b = new GreenApple('middle', false);
+const c = new GreenApple('large', false);
 
-console.log(a);
+console.log(a, b, c, a === b);
+
+b.size = 'small';
+
+console.log(a.size, b.size, a === b, a.size === b.size);
+
+a.fall();
+b.fall();
