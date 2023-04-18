b75c7ff28569273bfb732324aee8685450634e39
Artem Koziar <koziar.artem@gmail.com>
2023-04-18 20:09:40 +0300

    oop-2: array prototype

--- index.js
+++ index.js
@@ -1,72 +1,13 @@
-`use strict`;
+'use strict';
 
-class Plant {
-    constructor(color, size) {
-        this.color = color;
-        this.size = size;
-    }
+const list = [1, 2, 3, 4, 5]
 
-    cut() {
-        console.log('Cutting successful ' + this.color);
-    }
+// const shuffle = (arr) => arr.sort(() => Math.round(Math.random() * 2) - 1);
 
-    setSize(size) {
-        if (['small', 'middle', 'large'].includes(size)) {
-            this.size = size;
-        } else {
-            throw new Error('Wrong size');
-        }
-    }
+function shuffle() {
+    this.sort(() => Math.round(Math.random() * 2) - 1);
 }
 
-class Apple extends Plant {
-    constructor(color, size, isInTree) {
-        super(color, size);
-        this.isInTree = isInTree;
-    }
+list.shuffle = shuffle;
 
-    fall() {
-        if (this.isInTree) {
-            console.log(this.color + ' Apple fell');
-        } else {
-            console.log(this.color + ' Apple is not in tree');
-        }
-    }
-}
-
-class Tomato extends Plant {
-    constructor(color, size) {
-        super(color, size);
-    }
-
-    jump() {
-        console.log("I'm funny jumping tomato");
-    }
-}
-
-const a = new Apple('green', 'small', true);
-const b = new Tomato('red', 'middle', false);
-const c = new Apple('red', 'large', false);
-console.log(a, b, c, a === b);
-
-const size = document.getElementById('size') ?? 'small';
-
-b.setSize(size);
-
-console.log(a.size, b.size, a === b, a.size === b.size);
-
-a.fall();
-a.cut();
-b.cut();
-
-console.log('2' + '2');
-
-const margeHeight = (obj, height) => {
-    obj.height = height;
-    return obj;
-};
-
-const aa = margeHeight(a, 10);
-const bb = margeHeight(b, 20);
-
-console.log(aa, bb);
+Array.prototype.shuffle = shuffle;
