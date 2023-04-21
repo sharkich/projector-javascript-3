6a31985ff664fe63745cff00610c10245f5bc425
Artem Koziar <koziar.artem@gmail.com>
2023-04-21 20:21:02 +0300

    oop-workshop: add validation

--- classes/Form.js
+++ classes/Form.js
@@ -1,9 +1,24 @@
 export class Form {
-    isValid = false;
-    data = {
-        name: '',
-        author: '',
-        year: '',
-        isbn: '',
-    };
+    constructor({getName, getAuthor, getYear, getIsbn}) {
+        this.getName = getName;
+        this.getAuthor = getAuthor;
+        this.getYear = getYear;
+        this.getIsbn = getIsbn;
+    }
+
+    isValid () {
+        return !!this.getName()
+            && !!this.getAuthor()
+            && !!this.getYear()
+            && !!this.getIsbn();
+    }
+
+    getData () {
+        return {
+            name: '',
+            author: '',
+            year: '',
+            isbn: '',
+        }
+    }
 }
--- index.js
+++ index.js
@@ -7,18 +7,25 @@ import {Book} from "./classes/Book.js";
 
 console.log('log');
 
-const form = new Form();
+const form = new Form({
+    getName: () => document.getElementById('book_name').value,
+    getAuthor: () => document.getElementById('book_author').value,
+    getYear: () => document.getElementById('book_year').value,
+    getIsbn: () => document.getElementById('book_isbn').value,
+});
 const table = new Table();
 const messages = new Messages();
 
 document.getElementById('form').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    if (form.isValid) {
-        const book = new Book(form.data);
-        table.addBook(book);
-        messages.addSuccess(`Book ${book.name} added!`);
-    } else {
-        messages.addFail('Invalid form');
-    }
+    console.log(form.isValid());
+
+    // if (form.isValid()) {
+    //     const book = new Book(form.getData());
+    //     table.addBook(book);
+    //     messages.addSuccess(`Book ${book.name} added!`);
+    // } else {
+    //     messages.addFail('Invalid form');
+    // }
 });
