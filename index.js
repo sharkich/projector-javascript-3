d3a2e1ddd1ec271fc55b61de574b79cee0fa4307
Artem Koziar <koziar.artem@gmail.com>
2023-05-01 20:28:59 +0300

    lesson-18: Try/Catch

--- index.js
+++ index.js
@@ -27,24 +27,58 @@
 
 // Async/Await
 
-async function f1(id) {
-    const movieResponse = await fetch(`https://swapi.dev/api/films/${id}/`);
-    const movie = await movieResponse.json();
-    const promises = movie.characters.map((url) => {
-        return fetch(url).then(resp => resp.json());
-    });
-    const characters = await Promise.all(promises);
-    return characters.map(({name}) => name);
+// async function f1(id) {
+//     const movieResponse = await fetch(`https://swapi.dev/api/films/${id}/`);
+//     const movie = await movieResponse.json();
+//     const promises = movie.characters.map((url) => {
+//         return fetch(url).then(resp => resp.json());
+//     });
+//     const characters = await Promise.all(promises);
+//     return characters.map(({name}) => name);
+// }
+//
+//
+// const f2 = async () => {
+//     const names = await f1(1);
+//     console.log(names);
+// }
+//
+//
+// f1(1)
+//     .then((result) => console.log(result));
+
+
+// console.log(1);
+// const err = new Error('Some error');
+// console.log('name ->', err.name);
+// console.log('message ->', err.message);
+// throw err;
+// console.log(a);
+// console.log(null.length);
+// new Array(10000000000000)
+
+// Function sum of two numbers
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Arguments must be numbers');
+    }
+    return a + b;
 }
 
-
-const f2 = async () => {
-    const names = await f1(1);
-    console.log(names);
+function loadingData() {
+    console.log('app start');
+    console.log(sum(1, '2'));
+    console.log('app finish');
 }
 
+function tryLoadingData() {
+    try {
+        loadingData();
+    } catch (error) {
+        console.log('error ->', error.name, ':', error.message);
+    } finally {
+        console.log('finally');
+    }
+}
 
-f1(1)
-    .then((result) => console.log(result));
-
-
+tryLoadingData();
