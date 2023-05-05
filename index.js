76282e6b4b706aaf25639a1ea7a28bb0d431021b
Artem Koziar <koziar.artem@gmail.com>
2023-05-05 20:18:09 +0300

    workshop-4: fetch data and render

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 class UI {
-    constructor(searchUserInputElement, formElement) {
+    constructor(searchUserInputElement, formElement, profileElement) {
         this.searchUserInputElement = searchUserInputElement;
         this.formElement = formElement;
+        this.profileElement = profileElement;
         this.searchUserInput = '';
     }
 
@@ -23,7 +24,32 @@ class UI {
         });
     }
 
-    renderUserData(data) {
+    renderUserData(user) {
+        this.profileElement.innerHTML = `
+      <div class="card card-body mb-3">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          </div>
+          <div class="col-md-9">
+            <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+            <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+            <span class="badge badge-success">Followers: ${user.followers}</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+            <ul class="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h3 class="page-heading mb-3">Latest Repos</h3>
+      <div class="repos"></div>
+    `;
     }
 
     renderError(error) {
@@ -31,13 +57,16 @@ class UI {
 }
 
 class API {
-    async getUserData() {
+    async getUserData(input) {
+        const response = await fetch(`https://api.github.com/users/${input}`);
+        return await response.json();
     }
 }
 
 const ui = new UI(
     document.querySelector('.searchUser'),
     document.getElementById('form'),
+    document.getElementById('profile'),
 );
 const api = new API();
 
@@ -47,7 +76,7 @@ const run = () => {
     const searchUser = async (input) => {
         try {
             console.log('input ->', input);
-            const userData = await api.getUserData();
+            const userData = await api.getUserData(input);
             console.log('userData ->', userData);
             ui.renderUserData(userData);
         } catch (error) {
