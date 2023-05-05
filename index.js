73f737ba54825558a6985313ab5f8ace79cbd625
Artem Koziar <koziar.artem@gmail.com>
2023-05-05 19:58:16 +0300

    workshop-4: add addEventListener

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 'use strict';
 
-console.log('start');
+class UI {
+    constructor(searchUserInputElement) {
+        this.searchUserInputElement = searchUserInputElement;
+        this.searchUserInput = '';
+    }
+
+    onInputChange(callback) {
+        this.searchUserInputElement.addEventListener('keyup', (event) => {
+            this.searchUserInput = event.target.value;
+            callback(this.searchUserInput);
+        });
+    }
+
+    renderUserData(data) {
+    }
+
+    renderError(error) {
+    }
+}
+
+class API {
+    async getUserData() {
+    }
+}
+
+const ui = new UI(
+    document.querySelector('.searchUser'),
+);
+const api = new API();
+
+const run = () => {
+    console.log('start');
+    ui.onInputChange(async (input) => {
+        console.log('try', input);
+        try {
+            const userData = await api.getUserData();
+            ui.renderUserData(userData);
+        } catch (error) {
+            ui.renderError(error);
+        }
+    });
+};
+
+run();
