e4ca8b4b518d0f26feee7fb5ae3693555d3399a3
Artem Koziar <koziar.artem@gmail.com>
2023-05-05 20:09:34 +0300

    workshop-4: v1

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
 
 class UI {
-    constructor(searchUserInputElement) {
+    constructor(searchUserInputElement, formElement) {
         this.searchUserInputElement = searchUserInputElement;
+        this.formElement = formElement;
         this.searchUserInput = '';
     }
 
     onInputChange(callback) {
         this.searchUserInputElement.addEventListener('keyup', (event) => {
             this.searchUserInput = event.target.value;
+            if (callback) {
+                callback(this.searchUserInput);
+            }
+        });
+    }
+
+    onFormSubmit(callback) {
+        this.formElement.addEventListener('submit', (event) => {
+            event.preventDefault();
             callback(this.searchUserInput);
         });
     }
@@ -27,20 +37,26 @@ class API {
 
 const ui = new UI(
     document.querySelector('.searchUser'),
+    document.getElementById('form'),
 );
 const api = new API();
 
 const run = () => {
     console.log('start');
-    ui.onInputChange(async (input) => {
-        console.log('try', input);
+
+    const searchUser = async (input) => {
         try {
+            console.log('input ->', input);
             const userData = await api.getUserData();
+            console.log('userData ->', userData);
             ui.renderUserData(userData);
         } catch (error) {
             ui.renderError(error);
         }
-    });
+    };
+
+    ui.onInputChange();
+    ui.onFormSubmit(searchUser);
 };
 
 run();
