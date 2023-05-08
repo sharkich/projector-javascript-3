59f778918549be7573c20d607b3b54423caa5afa
Artem Koziar <koziar.artem@gmail.com>
2023-05-08 20:05:20 +0300

    workshop-5: add subscribeToInputChange

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 `use strict`;
 
-console.log('start');
+const API_KEY = '92b9bb363abdb27bec098afa3ba778e5';
+const q = 'Kyiv';
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=${API_KEY}&units=metric`;
+console.log(URL);
+
+class UI {
+    #formElement = null;
+    #inputElement = null;
+    #input = '';
+
+    constructor(formElement, inputElement) {
+        this.#formElement = formElement;
+        this.#inputElement = inputElement;
+
+        this.#subscribeToInputChange();
+    }
+
+    #subscribeToInputChange() {
+        this.#inputElement.addEventListener('keyup', (event) => {
+            this.#input = event.target.value;
+        });
+    }
+
+    onSubmit(callback) {
+        this.#formElement.addEventListener('submit', (event) => {
+            event.preventDefault();
+            callback(this.#input);
+        });
+    }
+
+    renderCityWeather(cityWeather) {}
+}
+
+class API {
+    getCityWeather(cityName) {}
+}
+
+const ui = new UI(
+    document.getElementById('form'),
+    document.getElementById('input'),
+);
+const api = new API();
+
+const run = () => {
+    console.log('run');
+    ui.onSubmit(async (cityName) => {
+        console.log('onSubmit', cityName);
+        const cityWeather = await api.getCityWeather(cityName);
+        ui.renderCityWeather(cityWeather);
+    });
+};
+
+run();
