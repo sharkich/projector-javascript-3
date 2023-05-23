5572f2175acd63f9dacf9abe9e9e7e32981a34dd
Artem Koziar <koziar.artem@gmail.com>
2023-05-23 19:42:24 +0300

    Add X

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('start');
+
+const buttonsElements = document.querySelectorAll('.box');
+
+const addEventListener = (buttonElement) => {
+    buttonElement.addEventListener('click', () => {
+        console.log('click');
+        buttonElement.innerHTML = 'X';
+    });
+};
+
+buttonsElements.forEach(addEventListener);
