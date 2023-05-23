6764469619f72bdb8f07de0c3ab5a9f0bbc654d4
Artem Koziar <koziar.artem@gmail.com>
2023-05-23 20:13:40 +0300

    Add isWin

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 console.log('start');
 
 const buttonsElements = document.querySelectorAll('.box');
+const [e0, e1, e2, e3, e4, e5, e6, e7, e8] = buttonsElements;
+const GROUPS = [
+    [e0, e1, e2],
+    [e3, e4, e5],
+    [e6, e7, e8],
+
+    [e0, e3, e6],
+    [e1, e4, e7],
+    [e2, e5, e8],
+
+    [e0, e4, e8],
+    [e2, e4, e6],
+];
 
 const PLAYERS = {
     PLAYER_1: 'X',
@@ -13,6 +26,13 @@ const generateButtonSymbol = () => {
     return isLastPlayer1 ? PLAYERS.PLAYER_2 : PLAYERS.PLAYER_1;
 }
 
+const isWin = () => GROUPS
+    .some(([e1, e2, e3]) =>
+        !!e1.innerText &&
+        e1.innerText === e2.innerText &&
+        e2.innerText === e3.innerText
+    );
+
 const addEventListener = (buttonElement) => {
     buttonElement.addEventListener('click', () => {
         console.log('click');
@@ -20,6 +40,11 @@ const addEventListener = (buttonElement) => {
             return;
         }
         buttonElement.innerHTML = generateButtonSymbol();
+        if (isWin()) {
+            setTimeout(() => {
+                alert('win');
+            }, 0);
+        }
     });
 };
 
