31dee65185fe15409b88cee7b2f7f7e6d8b55fd0
Artem Koziar <koziar.artem@gmail.com>
2023-05-23 19:55:20 +0300

    Add O

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ console.log('start');
 
 const buttonsElements = document.querySelectorAll('.box');
 
+const PLAYERS = {
+    PLAYER_1: 'X',
+    PLAYER_2: 'O',
+};
+
+let isLastPlayer1 = true;
+const generateButtonSymbol = () => {
+    isLastPlayer1 = !isLastPlayer1;
+    return isLastPlayer1 ? PLAYERS.PLAYER_2 : PLAYERS.PLAYER_1;
+}
+
 const addEventListener = (buttonElement) => {
     buttonElement.addEventListener('click', () => {
         console.log('click');
-        buttonElement.innerHTML = 'X';
+        buttonElement.innerHTML = generateButtonSymbol();
     });
 };
 
